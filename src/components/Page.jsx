a86d4e685e9ed4df7261a86a5b15fbe4350ca447
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-11-30 00:45:41 +0545

    homesection

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Header from "./common/header/Header";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./home/Home";
 
 const Page = () => {
   return (
@@ -7,6 +9,7 @@ const Page = () => {
       {/* <h1>hello</h1> */}
       {/* Header homre about services blog procing contact */}
       <Header />
+      <Home />
     </>
   );
 };
