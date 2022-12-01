f712a7981bcdb8954b250d33391b2cdc7ce577b5
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-01 21:30:48 +0545

    recent section started

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Featured from "../featured/Featured";
 import Hero from "../hero/Hero";
+import Recent from "../recent/Recent";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <Featured />
+      <Recent />
     </>
   );
 };
---
+++ src/components/recent/RecentCard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RecentCard = () => {
+  return (
+    <>
+      <h1>recent card</h1>
+    </>
+  );
+};
+
+export default RecentCard;
