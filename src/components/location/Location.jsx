f1bf88e66e7f27fd0df918313a1c43754d0da1a1
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-02 22:58:32 +0545

    Loacation design html complete

--- src/components/location/Location.jsx
+++ src/components/location/Location.jsx
@@ -1,9 +1,37 @@
 import React from "react";
+import Heading from "../common/Heading";
+import { location } from "../data/Data";
 
 const Location = () => {
   return (
     <>
-      <h1>hello from location</h1>
+      <section className="location padding">
+        <div className="container">
+          <Heading
+            title="Explore By Location"
+            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <div className="content grid3 mtop">
+            {location.map((val, index) => {
+              return (
+                <>
+                  <div className="box" key={index}>
+                    <img src={val.cover} alt="" />
+                    <div className="overlay">
+                      <h5>{val.name}</h5>
+                      <p>
+                        <label>{val.Villas}</label>
+                        <label>{val.Offices}</label>
+                        <label>{val.Apartments}</label>
+                      </p>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
