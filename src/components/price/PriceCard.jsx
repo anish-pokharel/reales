b60307cc66689c42f04e8e68a7fbd2ab874e5b3c
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-03 22:50:09 +0545

    price section

--- src/components/price/PriceCard.jsx
+++ src/components/price/PriceCard.jsx
@@ -1,7 +1,52 @@
 import React from "react";
+import { price } from "../data/Data";
 
 const PriceCard = () => {
-  return <h2>hello</h2>;
+  return (
+    <>
+      <div className="content flex mtop">
+        {price.map((val, index) => {
+          return (
+            <>
+              <div className="box shadow" key={index}>
+                <div className="topbtn">
+                  <button className="btn3">{val.best}</button>
+                </div>
+                <h3>{val.plan}</h3>
+                <h1>
+                  <span>$</span>
+                  {val.price}
+                </h1>
+                <p>{val.ptext}</p>
+
+                <ul>
+                  {val.list.map((value) => {
+                    const { icon, text, change } = value;
+                    return (
+                      <>
+                        <li>
+                          <label
+                            style={{
+                              background:
+                                change === "color" ? "#dc35451f" : "#27ae601f",
+                              color: change === "color" ? "#dc3848" : "#27ae60",
+                            }}
+                          >
+                            {icon}
+                          </label>
+                          <p>{text}</p>
+                        </li>
+                      </>
+                    );
+                  })}
+                </ul>
+              </div>
+            </>
+          );
+        })}
+      </div>
+    </>
+  );
 };
 
 export default PriceCard;
