e64036ba235f28a0744ec52912b965a4e04c6631
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-03 20:14:28 +0545

    team html completed

--- src/components/team/Team.jsx
+++ src/components/team/Team.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import Heading from "../../components/common/Heading";
+import { team } from "../data/Data";
 
 const Team = () => {
   return (
     <>
-      <h1>hello from team</h1>
+      <section className="team background">
+        <div className="container">
+          <Heading
+            title="Our Featured Agents"
+            subtitle="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
+          />
+          <div className="content mtop grid3">
+            {team.map((val, index) => {
+              return (
+                <>
+                  <div className="box" key={index}>
+                    <button className="btn3">{val.list}Listing</button>
+                    <div className="details">
+                      <div className="img">
+                        <img src={val.cover} alt="" />
+                        <i className="fa-solid fa-circle-check"></i>
+                      </div>
+                      <i className="fa fa-location-dot"></i>
+                      <label>{val.address}</label>
+                      <h4>{val.name}</h4>
+                      <ul>
+                        {val.icon.map((icon, index) => {
+                          return (
+                            <>
+                              <li key={index}>{icon}</li>
+                            </>
+                          );
+                        })}
+                      </ul>
+                      <div className="button flex">
+                        <button>
+                          <i className="fa fa-envelope"></i>Message
+                        </button>
+                        <button className="btn4">
+                          <i className="fa fa-phone-alt"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
