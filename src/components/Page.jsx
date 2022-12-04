43936bb070f2f3eb80d3ce88b9ccb11e7762b789
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-04 23:53:01 +0545

    about routing

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -3,6 +3,7 @@ import Header from "./common/header/Header";
 import Footer from "./footer/Footer";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./home/Home";
+import About from "./about/About";
 
 const Page = () => {
   return (
@@ -17,7 +18,7 @@ const Page = () => {
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
           <Route path="/" element={<Home />} />
           <Route path="/" element={<Home />} />
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Back from "../common/Back";
+const About = () => {
+  return (
+    <section className="about">
+      <Back name="About Us" title="About Us - Who We Are?" cover={img} />
+    </section>
+  );
+};
+
+export default About;
